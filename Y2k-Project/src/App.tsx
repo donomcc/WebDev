8837c45394c05dbbe0ac89d9bb6385fdcbecd935
Donovan McCollum <donomcc@gmail.com>
2025-10-04 01:20:46 -0500

    css setup
    update css and readme.md

--- Y2k-Project/src/App.tsx
+++ Y2k-Project/src/App.tsx
@@ -1,8 +1,33 @@
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="y2k-container">
+      <div className="background-texture"></div>
+      <div className="github">
+        <p>
+          <a target="_blank" href="https://github.com/donomcc">
+            [GITHUB]
+          </a>
+          <br />
+          System Status: Online.
+          <br />
+          Initializing Protocol...
+        </p>
+      </div>
+      <div className="linkedin">
+        <p>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/donovanmccollum/"
+          >
+            [LINKEDIN]
+          </a>
+          <br />
+          System Status: Online.
+          <br />
+          Transmitting Data...
+        </p>
+      </div>
+    </div>
   );
 }
 
