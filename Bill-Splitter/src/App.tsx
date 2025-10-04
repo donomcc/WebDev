61bf0c2bf90d38e9dfa3d54a4deb96bf3efc01fe
Donovan McCollum <donomcc@gmail.com>
2025-10-03 22:12:19 -0500

    creates base layout
    creates base layout html and CSS

--- Bill-Splitter/src/App.tsx
+++ Bill-Splitter/src/App.tsx
@@ -1,5 +1,44 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="card">
+        <div className="container">
+          <div className="title">
+            <h1>Bill Splitter</h1>
+          </div>
+          <div className="input">
+            <form>
+              <div className="bills">
+                <label className="billsLabel" htmlFor="total">
+                  Total Bill
+                </label>
+                <i className="fa-solid fa-dollar-sign"></i>
+                <input type="text" name="total" />
+              </div>
+              <div className="people">
+                <label className="peopleLabel" htmlFor="people">
+                  Number of People
+                </label>
+                <i className="fa-solid fa-user"></i>
+                <input type="text" name="people" />
+              </div>
+              <div className="tip">
+                <label className="tipLabel" htmlFor="tip">
+                  Tip Percentage
+                </label>
+                <i className="fa-solid fa-hand-holding-dollar"></i>
+                <input type="text" name="tip" />
+              </div>
+            </form>
+          </div>
+          <div className="results">
+            <h2>Results</h2>
+            <h4>Amount Per Person: </h4>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
